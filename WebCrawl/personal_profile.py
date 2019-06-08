ff60d550c1064023581c5c3fdf93c2e69f8a9bbd
neltia <33018575+neltia@users.noreply.github.com>
2019-06-08 17:17:49 +0900

    Update personal_profile.py

--- WebCrawl/personal_profile.py
+++ WebCrawl/personal_profile.py
@@ -11,8 +11,10 @@ if(emot == 'pos'):
 	emots = '긍정'
 elif(emot == 'neg'):
 	emots = '부정'
-else:
+elif(emot == 'nat'):
 	emots = '중립'
+else:
+	emots = 'None'
 
 #크롤링 분석 완료
 user_profile = [names]
@@ -24,11 +26,16 @@ user_profile['emotion'] = emot
 
 pprint(user_profile)
 print("%s님이 매체를 주로 이용하는 시각은 %d시 입니다."%(names, key_max))
-print("%s님의 관심사는 본문에서 %s번 빈도가 나타난 \"%s\"입니다."%(names, pointlist[1], fword[0]))
-print("%s님이 주제에 대해 주로 나타내는 성향은 %s입니다."%(names, emots))
 
-plt.bar(fxs, fnumber)
-plt.ylabel("단어 수")
-plt.title("단어 계산")
-plt.xticks([i + 0.5 for i, _ in enumerate(fword)], fword, rotation = 90)
-plt.show()
+if iH:
+	print("%s님의 관심사는 본문에서 %s번 빈도가 나타난 \"%s\"입니다."%(names, pointlist[1], fword[0]))
+	print("%s님이 주제에 대해 주로 나타내는 성향은 %s입니다."%(names, emots))
+	plt.bar(fxs, fnumber)
+	plt.ylabel("단어 수")
+	plt.title("단어 계산")
+	plt.xticks([i + 0.5 for i, _ in enumerate(fword)], fword, rotation = 90)
+	plt.show()
+if not iH:
+	print("관심사 is %s"%(fword[0]))
+	print("주제 성향 is %s입니다."%(emots))
+	print("Graph is None")
