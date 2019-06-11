a17c47e0563b8eb97764b4811d30c065f11d4cb2
neltia <33018575+neltia@users.noreply.github.com>
2019-06-12 01:28:55 +0900

    Update personal_feeling.py

--- total/personal_feeling.py
+++ total/personal_feeling.py
@@ -46,13 +46,13 @@ def naive_bayes_classifier(test, train, all_count):
         result *= float(round(list_naive[i], 6))
     return float(result)*float(1.0/3.0)
 
-def mainfeel():
+def mainfeel(f_testd):
     global emot
     # get the data
     f_pos = open('positive.txt', 'r',encoding='UTF-8')
     f_neg = open('negative.txt', 'r',encoding='UTF-8')
     f_neu = open('neutral.txt', 'r',encoding='UTF-8')
-    f_test = open('train_docs.txt','r',encoding='UTF-8')
+    f_test = f_testd
      
     # tag list (보통명사, 동사, 형용사, 보조동사, 명사추정범주) 
     # 참고 : https://docs.google.com/spreadsheets/d/1OGAjUvalBuX-oZvZ_-9tEfYD2gQe7hTGsgUpiiBSXI8/edit#gid=0
@@ -78,6 +78,7 @@ def mainfeel():
     list_neutral = getting_list(f_neu, list_neutral)
      
     ALL = len(set(list_positive))+len(set(list_negative))+len(set(list_neutral)) #전체 카운트, 함수에 들어가야한다. (all_count)
+    print(ALL)
 
     # naive bayes 값 계산
     result_pos = naive_bayes_classifier(test_output, list_positive, ALL)
@@ -86,13 +87,13 @@ def mainfeel():
      
     if (result_pos > result_neg and result_pos > result_neu):
         #print('긍정')
-        emot = 'pos'
+        emot = '긍정'
     elif (result_neg > result_pos and result_neg > result_neu):
         #print ('부정')
-        emot = 'neg'
+        emot = '부정'
     else:
         #print ('중립')
-        emot = 'nat'
+        emot = '중립'
      
     '''
     pprint(result_pos)
@@ -104,4 +105,4 @@ def mainfeel():
     f_neu.close()
     f_test.close()
 
-mainfeel()
+    return emot
