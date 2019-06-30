11ed7f365b8c9839037569eb2d6abca3d5026070
neltia <33018575+neltia@users.noreply.github.com>
2019-07-01 00:33:10 +0900

    Update spst_analysis.py

--- spst/spst_analysis.py
+++ spst/spst_analysis.py
@@ -5,24 +5,36 @@ import tkinter.ttk
 from tkinter import messagebox
 import pandas as pd
 import os
+import pymysql
 
-#데이터베이스 로딩
-if(platform.system()=='Windows'):
-	pathvar = os.path.dirname( os.path.abspath( __file__ ) ).split('\\')[2]
-	conf = sqlite3.connect("C:/Users/"+ pathvar + "/Users.db")
-	cursor = conf.cursor()
-elif(platform.system()=='Darwin'):
-	file = "Users.db"
-	conf = sqlite3.connect(file)
-	cursor = conf.cursor()
+try:
+    #데이터베이스 서버 연결
+    conf = pymysql.connect(host='106.10.32.85', 
+                        user='root', 
+                        passwd='shsmsrpwhgdktjqjdlqslek!', 
+                        db='SPST_S',
+                        charset='utf8',
+                        port=3306,
+                        )
+    cursor = conf.cursor()
+except:
+    #데이터베이스 로딩
+    if(platform.system()=='Windows'):
+    	pathvar = os.path.dirname( os.path.abspath( __file__ ) ).split('\\')[2]
+    	conf = sqlite3.connect("C:/Users/"+ pathvar + "/SPST_S.db")
+    	cursor = conf.cursor()
+    elif(platform.system()=='Darwin'):
+    	file = "SPST_S.db"
+    	conf = sqlite3.connect(file)
+    	cursor = conf.cursor()
 
-def inter(ids):
+def inter(IDs):
 	f = open("analysid.txt","w")
-	data = ids
+	data = IDs
 	f.write(data)
 	f.close()
 
-	#os.system("spst_analy_crawl.exe")
+	os.system("spst_analy_crawl.exe")
 
 def my_table(self):
 	treeview.tag_configure("tag2", background="red")
@@ -34,7 +46,7 @@ class MyFrames(Frame):
         self.master = master
         self.pack(fill=BOTH, expand=True)
 
-        #id
+        #ID
         frame1 = Frame(self)
         frame1.pack(fill=X)
         lblId = Label(frame1, text ="분석할 아이디",width=10)
@@ -50,60 +62,60 @@ class MyFrames(Frame):
 
 
 #사원 관리 메인
-global x0, y0,hides
+global x0, y0,hIDes
 analys= Tk()
 analys.resizable(0, 0)
 analys.title("훈련관리")
 x0, y0 = 820, 490
-width3,height3 = x0+20,y0+30
-screen3_wid = analys.winfo_screenwidth()
+wIDth3,height3 = x0+20,y0+30
+screen3_wID = analys.winfo_screenwidth()
 screen3_hei = analys.winfo_screenheight()
-x3 = ((screen3_wid/2) - (width3/2))
+x3 = ((screen3_wID/2) - (wIDth3/2))
 y3 = (screen3_hei/2) - (height3/2)
-analys.geometry('%dx%d+%d+%d' % (width3, height3, x3, y3))
+analys.geometry('%dx%d+%d+%d' % (wIDth3, height3, x3, y3))
 
-userlist = pd.read_sql("SELECT * FROM user_info",conf)
-userlist = list(userlist["id"])
+userlist = pd.read_sql("SELECT * FROM USER_INFO",conf)
+userlist = list(userlist["ID"])
 
-userset = pd.read_sql("SELECT * FROM user_analysis",conf)
+userset = pd.read_sql("SELECT * FROM USER_ANALYSIS",conf)
 cols = list(userset)
 
-ids = userset["id"].tolist()
-ats = userset["active_time"].tolist()
-sus = userset["subject"].tolist()
-ems = userset["emotion"].tolist()
+IDs = userset["ID"].tolist()
+ats = userset["ACTIVE_TIME"].tolist()
+sus = userset["SUBJECT"].tolist()
+ems = userset["EMOTION"].tolist()
 
 e_lbl = Label(analys,text="훈련 관리 테이블")
 e_lbl.pack()
 e_but = Label(analys,text="My_Table")
 e_but.pack()
 
-treeview=tkinter.ttk.Treeview(analys, columns=["one", "two","three","four"])
-treeview.column("#0", width=50)
-treeview.heading("#0",text="num") #index
-treeview.column("one", width=70,anchor="w")
-treeview.heading("one", text=cols[0],anchor="center") #id
-treeview.column("two", width=50,anchor="w")
-treeview.heading("two", text=cols[1],anchor="center") #active time
-treeview.column("three", width=100,anchor="w")
-treeview.heading("three", text=cols[2],anchor="center") #subject
-treeview.column("four", width=100, anchor="w")
-treeview.heading("four", text=cols[3], anchor="center") #emotion
+treeview2=tkinter.ttk.Treeview(analys, columns=["one", "two","three","four"])
+treeview2.column("#0", width=50)
+treeview2.heading("#0",text="num") #index
+treeview2.column("one", width=100,anchor="center")
+treeview2.heading("one", text=cols[0].lower(),anchor="center") #ID
+treeview2.column("two", width=50,anchor="center")
+treeview2.heading("two", text="time",anchor="center") #active time
+treeview2.column("three", width=80,anchor="center")
+treeview2.heading("three", text=cols[2].lower(),anchor="center") #subject
+treeview2.column("four", width=80, anchor="center")
+treeview2.heading("four", text=cols[3].lower(), anchor="center") #emotion
 
 treelists = []
-for x in range(len(ids)):
+for x in range(len(IDs)):
     treelist = []
-    treelist.append(ids[x])
+    treelist.append(IDs[x])
     treelist.append(ats[x])
     treelist.append(sus[x])
     treelist.append(ems[x])
     treelists.append(tuple(treelist))
 
 for i in range(len(treelists)):
-    treeview.insert('', 'end', text=i, values=treelists[i], iid=str(i)+"번")
+    treeview2.insert('', 'end', text=i, values=treelists[i], iid=str(i)+"번")
 
-treeview.tag_bind("tag1", sequence="<<TreeviewSelect>>", callback=my_table)
-treeview.pack(side=TOP,fill=X)
+treeview2.tag_bind("tag1", sequence="<<TreeviewSelect>>", callback=my_table)
+treeview2.pack(side="top",fill="x")
 
 try:
 	analys.iconbitmap(default='C:/Users/' + pathvar + '/Downloads/SPST_S-master/project_icon.ico')
